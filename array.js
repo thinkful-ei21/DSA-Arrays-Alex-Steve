13743b7aa87a5b3187a104f8511cac5891da4825
Steven Imus <imussg@gmail.com>
2018-07-25 12:34:33 -0400

    save point

--- array.js
+++ array.js
@@ -1,15 +1,73 @@
-import memory from './memory';
+const Memory = require('./memory');
 
 class Array {
 	
 	constructor() {
 		this.length = 0;
-		this.ptr = memory.allocate(this.length);
+		this._capacity = 0;
+        this.ptr = Memory.allocate(this.length);
 	}
 
 	push(value) {
-		this._resize(this.length + 1);
-		memory.set(this.ptr + this.length, value);
-		this.length++;
+		if (this.length >= this._capacity) {
+            this._resize((this.length + 1) * Array.SIZE_RATIO);
+        }
+
+        Memory.set(this.ptr + this.length, value);
+        this.length++;
 	}
+
+	_resize(size) {
+		const oldPtr = this.ptr;
+        this.ptr = Memory.allocate(size);
+        if (this.ptr === null) {
+            throw new Error('Out of Memory');
+        }
+        Memory.copy(this.ptr, oldPtr, this.length);
+        Memory.free(oldPtr);
+        this._capacity = size;
+	}
+
+	get(index) {
+        if (index < 0 || index >= this.length) {
+            throw new Error('Index error');
+        }
+        return Memory.get(this.ptr + index);
+    }
+
+    pop() {
+        if (this.length == 0) {
+            throw new Error('Index error');
+        }
+        const value = Memory.get(this.ptr + this.length - 1);
+        this.length--;
+        return value;
+    }
+
+    remove(index) {
+        if (index < 0 || index >= this.length) {
+            throw new Error('Index error');
+        }
+        Memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
+        this.length--;
+    }
+
 }
+Array.SIZE_RATIO = 3;
+
+function main() {
+    //create an instance of the array class
+    let arr = new Array();
+
+    //add an item to the array
+    arr.push(3);
+    arr.push(5);
+    arr.push(15);
+    arr.push(19);
+    arr.push(45);
+    arr.push(10);
+
+    console.log(arr);
+}
+
+main();
--- memory.js
+++ memory.js
@@ -1,4 +1,5 @@
 class Memory {
+
   constructor() {
     this.memory = new Float64Array(1024);
     this.head = 0;
